c6dea5f2130411f55e65180464c38a8adb196b66
jedrzej.kociecki@gmail.com <jedrzej.kociecki@gmail.com>
2024-12-30 00:35:57 +0100

    navigation, topped boulders

---
+++ cgm/Core/Map/MapFiltersView.swift
@@ -0,0 +1,18 @@
+//
+//  MapFiltersView.swift
+//  cgm
+//
+//  Created by Jędrzej Kocięcki on 29/12/2024.
+//
+
+import SwiftUI
+
+struct MapFiltersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapFiltersView()
+}
---
+++ cgm/Core/UICommon/RangeSlider.swift
@@ -0,0 +1,18 @@
+//
+//  RangeSlider.swift
+//  cgm
+//
+//  Created by Jędrzej Kocięcki on 29/12/2024.
+//
+
+import SwiftUI
+
+struct RangeSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RangeSlider()
+}
