8ec6adbb32ba56d085436116c7d4d6b3238b0264
jedrzej.kociecki@gmail.com <jedrzej.kociecki@gmail.com>
2025-01-03 17:40:46 +0100

    admin add boulder

---
+++ cgm/Core/GymOwner/AddBoulderView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBoulderView.swift
+//  cgm
+//
+//  Created by Jędrzej Kocięcki on 03/01/2025.
+//
+
+import SwiftUI
+
+struct AddBoulderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBoulderView()
+}
---
+++ cgm/Core/GymOwner/GymOwnerView.swift
@@ -0,0 +1,18 @@
+//
+//  GymOwnerView.swift
+//  cgm
+//
+//  Created by Jędrzej Kocięcki on 03/01/2025.
+//
+
+import SwiftUI
+
+struct GymOwnerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GymOwnerView()
+}
