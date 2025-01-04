190699cc864a16661a2ce1a98c538ff5b81166f5
jedrzej.kociecki@gmail.com <jedrzej.kociecki@gmail.com>
2025-01-04 16:47:50 +0100

    about us

---
+++ cgm/Core/AboutUs/AboutUsModel.swift
@@ -0,0 +1,8 @@
+//
+//  AboutUsModel.swift
+//  cgm
+//
+//  Created by Jędrzej Kocięcki on 04/01/2025.
+//
+
+import Foundation
---
+++ cgm/Core/AboutUs/GymInfo.swift
@@ -0,0 +1,18 @@
+//
+//  GymInfo.swift
+//  cgm
+//
+//  Created by Jędrzej Kocięcki on 04/01/2025.
+//
+
+import SwiftUI
+
+struct GymInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GymInfo()
+}
