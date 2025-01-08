9ecfe795acbc0cf8489039e74c674369965b870c
jedrzej.kociecki@gmail.com <jedrzej.kociecki@gmail.com>
2025-01-08 23:19:48 +0100

    loading animation

--- cgm/cgmApp.swift
+++ cgm/cgmApp.swift
@@ -10,36 +10,39 @@ import Supabase
 
 @main
 struct cgmApp: App {
-    @StateObject private var authmanager = AuthManager.shared
+    @StateObject private var authManager = AuthManager.shared
     @State private var isLoading = true
-
     
     var body: some Scene {
         WindowGroup {
-            Group{
-                if authmanager.isAuthenticated {
-                    MainView()
-                }else{
-                    RegisterView()
+            ZStack {
+                if isLoading {
+                    AnimatedLoader(size: 60)
+                        
+                } else {
+                    Group {
+                        if authManager.isAuthenticated {
+                            MainView()
+                        } else {
+                            RegisterView()
+                        }
+                    }
                 }
             }
             .onAppear {
-                           Task {
-                               async let authCheck = authmanager.checkAuth()
-                               async let artificialDelay = Task.sleep(for: .seconds(1.5))
-                               
-                               await (_, _) = (authCheck, try artificialDelay)
-                               
-                               await MainActor.run {
-                                   withAnimation {
-                                       isLoading = false
-                                   }
-                               }
-                               UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-
-                           }
-                       }
-
-                   }
-               }
+                Task {
+                    async let authCheck = authManager.checkAuth()
+                    async let artificialDelay = Task.sleep(for: .seconds(2))
+                    
+                    await (_, _) = (authCheck, try artificialDelay)
+                    
+                    await MainActor.run {
+                        withAnimation {
+                            isLoading = false
+                        }
+                    }
+                }
+            }
         }
+    }
+}
